5c053a6b038fba62052fa79adc7708b46ea91928
Jorge <jorge.groenke@gmail.com>
2018-12-11 17:42:40 +0000

    Set up pug and views - just index

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import '@babel/polyfill';
+import path from 'path';
 
 import photosRouter from './routes/photo';
 
@@ -8,12 +9,17 @@ dotenv.config();
 
 const app = express();
 
+// Set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(express.json());
 
+// Route handlers
 app.use('/photos', photosRouter);
 
 app.get('/', (req, res) => {
-  return res.status(200).send({ message: 'The endpoint worked!' });
+  return res.render('index');
 });
 
 app.listen(process.env.PORT, process.env.IP,() => console.log("photo app Server Started"));
