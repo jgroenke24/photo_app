1d296f5ae018544c378e7419328755f2bbd0b497
Jorge <jorge.groenke@gmail.com>
2018-12-14 18:07:30 +0000

    Add bootstrap and navbar

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>The react side has started...</p>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <h1>This is PicShareApp</h1>
+      </React.Fragment>
     );
   }
 }
--- client/app/index.js
+++ client/app/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './css/main.scss';
 import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('app')
 );
