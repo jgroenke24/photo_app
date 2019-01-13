83d660c1d39afdb69cecbe96d0ab71ba308f87ef
Jorge <jorge.groenke@gmail.com>
2019-01-13 01:14:50 +0000

    Add context to navbars and styling

--- client/app/components/MobileNavbar.js
+++ client/app/components/MobileNavbar.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppConsumer } from './AppContext';
 
 const MobileNavbar = (props) => (
-  <nav className='mobilenav'>
-    <ul className='mobilenav__links'>
-      <li>
-        <Link to='/'>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to='/dashboard'>
-          Dash
-        </Link>
-      </li>
-      <li>
-        <Link to='/upload'>
-          Upload
-        </Link>
-      </li>
-      <li>
-        <Link to='/search'>
-          Search
-        </Link>
-      </li>
-      <li>
-        <Link to='/profile'>
-          Profile
-        </Link>
-      </li>
-    </ul>
-  </nav>
+  <AppConsumer>
+    {({ isLoggedIn }) => (
+      <nav className='mobilenav'>
+        <ul className='mobilenav__links'>
+          <li>
+            <Link to='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/dashboard'>
+              Dash
+            </Link>
+          </li>
+          <li>
+            <Link to='/upload'>
+              Upload
+            </Link>
+          </li>
+          <li>
+            <Link to='/search'>
+              Search
+            </Link>
+          </li>
+          
+          {isLoggedIn &&
+            <li>
+              <Link to='/profile'>
+                Profile
+              </Link>
+            </li>
+          }
+        </ul>
+      </nav>
+    )}
+  </AppConsumer>
 );
 
 export default MobileNavbar;
--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -5,67 +5,55 @@ import { AppConsumer } from './AppContext';
 class Navbar extends Component {
   render() {
     return (
-      <header>
+      <Fragment>
         <AppConsumer>
         
           {({ isLoggedIn }) => (
-            <nav className='navbar navbar-expand-lg navbar-dark bg-primary fixed-top'>
-              <Link className='navbar-brand' to='/'>
-                PicShareApp
-              </Link>
-              <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-                <span className='navbar-toggler-icon'></span>
-              </button>
-            
-              <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                <ul className='navbar-nav mr-auto'>
-                  <li className='nav-item'>
-                    <NavLink className='nav-link' to='/'>
-                      Home 
-                      <span className='sr-only'>
-                        (current)
-                      </span>
-                    </NavLink>
-                  </li>
-                  <li className='nav-item'>
-                    <NavLink className='nav-link' to='/upload'>
-                      Upload
-                    </NavLink>
-                  </li>
-                  <li className='nav-item'>
-                    <NavLink className='nav-link' to='/dashboard'>
-                      Dashboard
-                    </NavLink>
-                  </li>
-                  
-                  {isLoggedIn ? (
-                    <Fragment>
-                      <li className='nav-item'>
-                        <NavLink className='nav-link' to='/user'>
-                          User Profile
-                        </NavLink>
-                      </li>
-                    </Fragment>
-                  ) : (
-                    <Fragment>
-                      <li className='nav-item'>
-                        <NavLink className='nav-link' to='/login'>
-                          Login
-                        </NavLink>
-                      </li>
-                      <li className='nav-item'>
-                        <NavLink className='nav-link' to='/signup'>
-                          Signup
-                        </NavLink>
-                      </li>
-                    </Fragment>
-                  )}
-                </ul>
-              </div>
+            <nav className='navbar' >
+              <ul className='navbar__links'>
+                <li className='navbar__item'>
+                  <NavLink to='/'>
+                    Home
+                  </NavLink>
+                </li>
+                <li className='navbar__item'>
+                  <NavLink to='/upload'>
+                    Upload
+                  </NavLink>
+                </li>
+                <li className='navbar__item'>
+                  <NavLink to='/dashboard'>
+                    Dashboard
+                  </NavLink>
+                </li>
+                
+                {isLoggedIn ? (
+                  <Fragment>
+                    <li className='navbar__item'>
+                      <NavLink to='/user'>
+                        User Profile
+                      </NavLink>
+                    </li>
+                  </Fragment>
+                ) : (
+                  <Fragment>
+                    <li className='navbar__item'>
+                      <NavLink to='/login'>
+                        Login
+                      </NavLink>
+                    </li>
+                    <li className='navbar__item'>
+                      <NavLink to='/signup'>
+                        Signup
+                      </NavLink>
+                    </li>
+                  </Fragment>
+                )}
+              </ul>
             </nav>
           )}
         </AppConsumer>
-      </header>
+      </Fragment>
     );
   }
 }
