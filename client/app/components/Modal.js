f4f31a9f716d0c8e38a456729c9c3c97bdd7d0b7
Jorge <jorge.groenke@gmail.com>
2019-01-24 23:00:29 +0000

    Change modal styles

--- client/app/components/Modal.js
+++ client/app/components/Modal.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactModal from 'react-modal';
 
 ReactModal.setAppElement('#app');
-ReactModal.defaultStyles.overlay.zIndex = 999;
+ReactModal.defaultStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    zIndex: 999,
+    overflow: 'auto',
+  },
+  content: {
+    minHeight: '100%',
+    padding: '1.5rem 4rem 6rem',
+    borderRadius: '0.25rem'
+  }
+};
 
 const Modal = (props) => {
   const { modalIsOpen, children } = props;
@@ -10,7 +26,11 @@ const Modal = (props) => {
   return (
     <ReactModal
       isOpen={modalIsOpen}
-      onRequestClose={() => props.history.goBack()}
+      onRequestClose={() => {
+        document.body.removeAttribute('style');
+        props.history.goBack();
+      }}
+      onAfterOpen={() => document.body.style.overflow = 'hidden'}
     >
       {children}
     </ReactModal>
