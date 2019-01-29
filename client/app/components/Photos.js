8d828c6e8d847743c79d1430d59016d05d701414
Jorge <jorge.groenke@gmail.com>
2019-01-29 01:16:10 +0000

    Fix pointer events on photo overlay

--- client/app/components/Photos.js
+++ client/app/components/Photos.js
@@ -26,34 +26,34 @@ const MultipleColumnPhotos = ({ photos, user }) => (
             className='photoitem__link'
           >
             <img src={url} alt={tags.replace(/,/g, ' ')} className='photoitem__img' />
-            <div className='photoitem__overlay'>
-              {user
-                ?  (
-                  <button className={'btn photoitem__btn ' + (likedByUser ? 'photoitem__btn--liked' : '')}>
-                    <span className='btn__icon'>
-                      <svg className='btn__icon--heart' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
-                        <path d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z'/>
-                      </svg>
-                    </span>
-                    {likes}
-                  </button>
-                ) : (
-                  <Link
-                    to='/login'
-                    className='btn photoitem__btn'
-                  >
-                    <span className='btn__icon'>
-                      <svg className='btn__icon--heart' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
-                        <path d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z'/>
-                      </svg>
-                    </span>
-                    {likes}
-                  </Link>
-                )
-              }
-              <h2 className='photoitem__creator'>{username}</h2>
-            </div>
           </Link>
+          <div className='photoitem__overlay'>
+            {user
+              ?  (
+                <button className={'btn photoitem__btn ' + (likedByUser ? 'photoitem__btn--liked' : '')}>
+                  <span className='btn__icon'>
+                    <svg className='btn__icon--heart' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+                      <path d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z'/>
+                    </svg>
+                  </span>
+                  {likes}
+                </button>
+              ) : (
+                <Link
+                  to='/login'
+                  className='btn photoitem__btn'
+                >
+                  <span className='btn__icon'>
+                    <svg className='btn__icon--heart' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+                      <path d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z'/>
+                    </svg>
+                  </span>
+                  {likes}
+                </Link>
+              )
+            }
+            <h2 className='photoitem__creator'>{username}</h2>
+          </div>
         </div>
       );
     })}
