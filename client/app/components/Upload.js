d318c60be810e465a6b98ab71f808f9ae95b95bd
Jorge <jorge.groenke@gmail.com>
2019-01-31 04:07:28 +0000

    Add picture preview to upload component when a file is selected with remove button

--- client/app/components/Upload.js
+++ client/app/components/Upload.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -15,6 +15,15 @@ class Upload extends Component {
     });
   }
   
+  handleRemoveFile = (event) => {
+    event.preventDefault();
+    this.setState(() => {
+      return {
+        file: null,
+      };
+    });
+  }
+  
   handleUpload = async (event) => {
     
     try {
@@ -28,7 +37,7 @@ class Upload extends Component {
       const data = new FormData();
       data.append('image', this.state.file, this.state.file.name);
       
-      document.querySelector('.form__progress').style.visibility = 'visible';
+      document.querySelector('.form__progress').style.cssText = 'visibility: visible; opacity: 1;';
       
       // Upload photo to cloudinary and save to database
       await axios
@@ -70,7 +79,7 @@ class Upload extends Component {
   }
   
   render() {
-    const { loaded, uploadError } = this.state;
+    const { file, loaded, uploadError } = this.state;
     return (
       <section className='form'>
       
@@ -88,13 +97,33 @@ class Upload extends Component {
         
         <form className='form__upload' onSubmit={this.handleUpload}>
           <div className='form__cutout'>
-            <label className='form__uplabel'>
-              <img className='form__img' src='https://png.pngtree.com/svg/20160809/camera_add_149556.png' alt='add image' />
-              <div className='btn'>
-                Browse
-              </div>
-              <input type='file' name='image' accept='image/*' className='form-control-file' onChange={this.handleFile} />
-            </label>
+          
+            {file ? (
+              <Fragment>
+                <img
+                  className='form__img'
+                  src={URL.createObjectURL(file)}
+                  alt='image preview'
+                />
+                <button
+                  className='form__remove'
+                  onClick={this.handleRemoveFile}
+                >
+                </button>
+              </Fragment>
+            ) : (
+              <label className='form__uplabel'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+                  <path d="M376.6 215.6V31.4c0-11.3-9.1-20.4-20.4-20.4H31.4C20.1 11 11 20.1 11 31.4v325.7c0 11.3 9.1 20.4 20.4 20.4h185.2C226.3 447.2 286 501 358.1 501c78.8 0 142.9-64.3 142.9-143.3 0-72.7-54.3-133-124.4-142.1zM51.8 51.8h283.9v107.4L267.4 98c-5.5-5.5-21.6-10.1-31.2 4.9l-67.5 115.4-19.8-34.3c-4.7-9.7-24.4-16.7-34.8-.9l-62.2 96.3V51.8zm11.5 284.9l66.8-103.3 59.7 103.3H63.3zM192.1 259l66.6-113.9 76.9 71.1c-52.9 8.4-96.2 45.9-113 95.6L192.1 259zM358 460.2c-56.3 0-102.1-46-102.1-102.5v-.5-.1c.3-56.2 46-101.9 102.1-101.9 56.3 0 102.1 46 102.1 102.4 0 56.6-45.8 102.6-102.1 102.6z"/>
+                  <path d="M411.1 337.3h-32.6v-32.6c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v32.6h-32.6c-11.3 0-20.4 9.1-20.4 20.4 0 11.3 9.1 20.4 20.4 20.4h32.6v32.6c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4v-32.6h32.6c11.3 0 20.4-9.1 20.4-20.4 0-11.3-9.1-20.4-20.4-20.4z"/>
+                  <circle cx="131.2" cy="109.2" r="21.4"/>
+                </svg>
+                <div className='btn'>
+                  Browse
+                </div>
+                <input type='file' name='image' accept='image/*' className='form-control-file' onChange={this.handleFile} />
+              </label>
+            )}
           </div>
           <div className='form__foot'>
             <div className='form__progress'>
