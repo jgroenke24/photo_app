92caf4e4c2261f449e67c40c5576fce3fd7b2d92
Jorge <jorge.groenke@gmail.com>
2019-02-17 19:37:12 +0000

    Add number of photos to profile component

--- client/app/components/Profile.js
+++ client/app/components/Profile.js
@@ -47,40 +47,45 @@ class Profile extends Component {
           
           {profileUser &&
             <Fragment>
-              <img
-                src={profileUser.avatar}
-                alt={`${profileUser.username} avatar`}
-                className='profile__image'
-              />
-              <div className='profile__info'>
-                <h1 className='profile__name'>
-                  {profileUser.firstName && profileUser.lastName
-                    ? `${profileUser.firstName} ${profileUser.lastName}`
-                    : profileUser.username
-                  }
-                </h1>
-              
-                {profileUser.bio ? (
-                  <p className='profile__bio'>{profileUser.bio}</p>
-                ) : (
-                  <p className='profile__bio'>
-                    Check out photos posted by
+              <div className='profile__body'>
+                <img
+                  src={profileUser.avatar}
+                  alt={`${profileUser.username} avatar`}
+                  className='profile__image'
+                />
+                <div className='profile__info'>
+                  <h1 className='profile__name'>
                     {profileUser.firstName && profileUser.lastName
-                      ? ` ${profileUser.firstName}`
-                      : ` ${profileUser.username}`
+                      ? `${profileUser.firstName} ${profileUser.lastName}`
+                      : profileUser.username
                     }
-                  </p>
-                )}
+                  </h1>
                 
-                {profileUser.location &&
-                  <div className='profile__location'>
-                    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' viewBox='0 0 24 24'>
-                      <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'/>
-                      <circle cx='12' cy='10' r='3'/>
-                    </svg>
-                    {profileUser.location}
-                  </div>
-                }
+                  {profileUser.bio ? (
+                    <p className='profile__bio'>{profileUser.bio}</p>
+                  ) : (
+                    <p className='profile__bio'>
+                      Check out photos posted by
+                      {profileUser.firstName && profileUser.lastName
+                        ? ` ${profileUser.firstName}`
+                        : ` ${profileUser.username}`
+                      }
+                    </p>
+                  )}
+                  
+                  {profileUser.location &&
+                    <div className='profile__location'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' viewBox='0 0 24 24'>
+                        <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'/>
+                        <circle cx='12' cy='10' r='3'/>
+                      </svg>
+                      {profileUser.location}
+                    </div>
+                  }
+                </div>
+              </div>
+              <div className='profile__footer'>
+                {photos ? `${photos.length} Photos` : `0 Photos`}
               </div>
             </Fragment>
           }
