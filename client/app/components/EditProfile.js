a3e54286e506897e8c1cb22fd2a86444d6edae6c
Jorge <jorge.groenke@gmail.com>
2019-02-21 16:04:12 +0000

    Add styles for mobile view of edit profile component

--- client/app/components/EditProfile.js
+++ client/app/components/EditProfile.js
@@ -4,55 +4,42 @@ class EditProfile extends Component {
   render() {
     return (
       <section className='edit'>
-        <h1>Edit Profile</h1>
-        <form>
-          <label>
+        <h1 className='edit__header'>Edit Profile</h1>
+        <form className='edit__picform'>
+          <label className='edit__piclabel'>
             <img
               src='https://smallbusinessbc.ca/wp-content/themes/sbbcmain/images/circle-icons/icon-education.svg'
               alt='avatar'
+              className='edit__avatar'
             />
-            <input type='file' name='image' accept='image/*' style={{display: 'none'}}/>
+            <input type='file' name='image' accept='image/*' />
             <p>Change profile image</p>
           </label>
         </form>
         
-        <form>
-          <label htmlFor='firstName'>First name</label>
-          <input id='firstName' type='text' name='firstName' />
+        <form className='edit__infoform'>
+          <label className='edit__infolabel' htmlFor='firstName'>First name</label>
+          <input className='edit__infoinput' id='firstName' type='text' name='firstName' />
           
-          <label htmlFor='lastName'>Last name</label>
-          <input id='lastName' type='text' name='lastName' />
+          <label className='edit__infolabel' htmlFor='lastName'>Last name</label>
+          <input className='edit__infoinput' id='lastName' type='text' name='lastName' />
           
-          <label htmlFor='email'>Email</label>
-          <input id='email' type='email' name='email' />
+          <label className='edit__infolabel' htmlFor='email'>Email</label>
+          <input className='edit__infoinput' id='email' type='email' name='email' />
           
-          <label htmlFor='username'>Username</label>
-          <input id='username' type='text' name='username' />
+          <label className='edit__infolabel' htmlFor='username'>Username</label>
+          <input className='edit__infoinput' id='username' type='text' name='username' />
           
-          <label htmlFor='location'>Location</label>
-          <input id='location' type='text' name='location' />
+          <label className='edit__infolabel' htmlFor='location'>Location</label>
+          <input className='edit__infoinput' id='location' type='text' name='location' />
           
-          <label htmlFor='bio'>Bio</label>
-          <div style={{position: 'relative'}}>
-            <textarea
-              id='bio'
-              name='bio'
-              rows='4'
-              style={{
-                display: 'block',
-                width: '100%',
-              }}
-            ></textarea>
-            <span
-              style={{
-                position: 'absolute',
-                right: '16px',
-                bottom: '16px',
-              }}
-            >150</span>
+          <label className='edit__infolabel' htmlFor='bio'>Bio</label>
+          <div className='edit__infotext'>
+            <textarea id='bio' name='bio' rows='4'></textarea>
+            <span>150</span>
           </div>
           
-          <input type='submit' value='Edit profile' />
+          <input className='btn form__btn' type='submit' value='Update profile' />
         </form>
       </section>
     );
