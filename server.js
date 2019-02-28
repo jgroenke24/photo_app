a0ef3aa92988d610c14e72ecc6ee88aa427c3b2f
Jorge <jorge.groenke@gmail.com>
2019-02-28 16:53:22 +0000

    Remove pug views folder and template engine in server file

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ const app = express();
 
 require('./config/passport');
 
-// Set up view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 // Increase security with helmet
 app.use(helmet());
 
