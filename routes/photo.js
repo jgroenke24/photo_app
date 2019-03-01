f5e70fc4bb05a5de3375b701f2d590b3ad7b867a
Jorge Groenke <jorge.groenke@gmail.com>
2019-02-28 23:42:21 -0500

    Set up webpack for server and scripts to run app

--- routes/photo.js
+++ routes/photo.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
   }
 });
 const imageFilter = (req, file, cb) => {
-  
+
     // accept image files only
     if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
         return cb(new Error('Only image files are allowed!'), false);
